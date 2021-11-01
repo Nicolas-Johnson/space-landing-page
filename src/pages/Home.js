bd6a579a8d2dab33257218b4ea9786ec3f94f22f
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-11-01 16:19:13 -0300

    Sass style initial setings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <Navigation />
       <div>
         <div>
-          <h1>SO, YOU WANT TO TRAVEL TO SPACE</h1>
+          <h5>SO, YOU WANT TO TRAVEL TO SPACE</h5>
           <span>Space</span>
           <p>
             Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
