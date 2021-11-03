9c880076560c38cec414738cfabeab1430ebf717
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-11-02 21:59:27 -0300

    Mobile Home style done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,18 +5,18 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
+    <div className="homeContainer">
       <Navigation />
-      <div>
-        <div>
-          <h5>SO, YOU WANT TO TRAVEL TO SPACE</h5>
-          <span>Space</span>
+      <div className="homecontent">
+        <div className="homeText">
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
           <p>
-            Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+            Let’s face it, if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
           </p>
         </div>
-        <div>
-          <button type="button"><Link to="/destination">Explore</Link></button>
+        <div className="homeBtn">
+          <button className="circleBtn" type="button"><Link className="linkBtn" to="/destination">EXPLORE</Link></button>
         </div>
       </div>
     </div>
